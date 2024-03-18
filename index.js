d3ca797d8a8141f664a92889b126deecd74cd578
promotie <153618979+promotie@users.noreply.github.com>
2024-03-18 16:45:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to /Help","Hosted By Medium Bots"];
+const statusMessages = ["Listening to 24/7 Uptime","Hosted By Medium Bots"];
 
 
 let currentIndex = 0;
